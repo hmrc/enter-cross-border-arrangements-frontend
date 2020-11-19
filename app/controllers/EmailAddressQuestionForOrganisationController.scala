4ea365bbf70b67e6fc32beed7a35bb6dab78e258
soljeggers <35451762+soljeggers@users.noreply.github.com>
2020-11-19 09:59:47 +0000

    DAC6-292 - refactored method

--- app/controllers/EmailAddressQuestionForOrganisationController.scala
+++ app/controllers/EmailAddressQuestionForOrganisationController.scala
@@ -83,7 +83,6 @@ class EmailAddressQuestionForOrganisationController @Inject()(
         value => {
 
           val determineRoute = (value, mode) match {
-            case (true, CheckMode) => false
             case (false, CheckMode) => true
             case  _ => false
           }
