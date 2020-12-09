a67a7621986cb5ed3616061c1f27f86470f693eb
helensoniega <48728381+helensoniega@users.noreply.github.com>
2020-12-09 17:13:01 +0000

    DAC6-417 Fixed error with nunjucks template

--- test/navigation/NavigatorSpec.scala
+++ test/navigation/NavigatorSpec.scala
@@ -34,7 +34,6 @@ import org.scalatestplus.scalacheck.ScalaCheckPropertyChecks
 import pages._
 import pages.arrangement._
 import pages.enterprises.{AssociatedEnterpriseTypePage, IsAssociatedEnterpriseAffectedPage, YouHaveNotAddedAnyAssociatedEnterprisesPage}
-import pages.enterprises.{AssociatedEnterpriseTypePage, IsAssociatedEnterpriseAffectedPage}
 import pages.hallmarks._
 import pages.individual._
 import pages.organisation._
