a4f0f849ed10f1103ad391d81d55551b9b5da4bf
iservport <992887+iservport@users.noreply.github.com>
2021-03-24 11:27:39 +0000

    DAC6-548 - Remove pre-populated answers in taxpayers & associated-enterprises journey

--- test/controllers/enterprises/SelectAnyTaxpayersThisEnterpriseIsAssociatedWithControllerSpec.scala
+++ test/controllers/enterprises/SelectAnyTaxpayersThisEnterpriseIsAssociatedWithControllerSpec.scala
@@ -167,7 +167,7 @@ class SelectAnyTaxpayersThisEnterpriseIsAssociatedWithControllerSpec extends Spe
       verify(mockRenderer, times(1)).render(templateCaptor.capture(), jsonCaptor.capture())(any())
       
       val expectedJson = Json.obj(
-        "form"       -> form,
+        "form"       -> filledForm,
         "mode"       -> NormalMode,
         "checkboxes" -> Checkboxes.set(field,
           Seq(Checkboxes.Checkbox(label = Literal(reporterDetailsAsOrganisation.nameAsString), value = s"${reporterDetailsAsOrganisation.nameAsString}")))
