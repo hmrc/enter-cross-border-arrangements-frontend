bb50aac3b9150ef9935412ffaf0c528692ab1b56
Maurício Fernandes de Castro <992887+iservport@users.noreply.github.com>
2021-06-29 13:07:30 +0100

    DAC6-925 - Missing format hint/inconsistent validation to be amended (#225)
    * DAC6-925 - Missing format hint/inconsistent validation to be amended
    
    * DAC6-925 - Missing format hint/inconsistent validation to be amended

--- app/forms/mappings/Formatters.scala
+++ app/forms/mappings/Formatters.scala
@@ -30,7 +30,7 @@ trait Formatters {
     override def bind(key: String, data: Map[String, String]): Either[Seq[FormError], String] = {
       data.get(key) match {
         case None | Some("") => Left(Seq(FormError(key, errorKey)))
-        case Some(s) => Right(s)
+        case Some(s) => Right(s.trim)
       }
     }
 
