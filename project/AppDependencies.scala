0f54bd43e505da0a58c2797d842b92e773952683
Mark Reynolds <20478032+ers2015112@users.noreply.github.com>
2022-11-24 15:34:04 +0000

    DAC6-2138: Update libraries (#276)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,30 +5,28 @@ object AppDependencies {
 
   val compile = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28"               % "0.68.0",
-    "uk.gov.hmrc"       %% "logback-json-logger"              % "5.1.0",
-    "uk.gov.hmrc"       %% "play-conditional-form-mapping"    % "1.9.0-play-28",
-    "uk.gov.hmrc"       %% "bootstrap-frontend-play-28"       % "5.24.0",
-    "uk.gov.hmrc"       %% "bootstrap-health-play-28"         % "5.3.0",
-    "uk.gov.hmrc"       %% "play-nunjucks"                    % "0.28.0-play-27",
-    "uk.gov.hmrc"       %% "play-nunjucks-viewmodel"          % "0.14.0-play-27",
-    "uk.gov.hmrc"       %% "play-frontend-hmrc"               % "3.0.0-play-28",
-    "uk.gov.hmrc"       %% "emailaddress"                     % "3.5.0",
-    "org.webjars.npm"   %  "govuk-frontend"                   % "3.12.0",
-    "org.webjars.npm"   %   "hmrc-frontend"                   % "1.35.0"
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28"               % "0.74.0",
+    "uk.gov.hmrc"       %% "play-conditional-form-mapping"    % "1.12.0-play-28",
+    "uk.gov.hmrc"       %% "bootstrap-frontend-play-28"       % "7.11.0",
+    "uk.gov.hmrc"       %% "play-nunjucks"                    % "0.40.0-play-28",
+    "uk.gov.hmrc"       %% "play-nunjucks-viewmodel"          % "0.16.0-play-28",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc"               % "3.33.0-play-28",
+    "org.webjars.npm"   %  "govuk-frontend"                   % "4.2.0",
+    "org.webjars.npm"   %   "hmrc-frontend"                   % "1.35.2"
   )
 
   val test = Seq(
-    "org.scalatest"               %% "scalatest"               % "3.2.9",
+    "org.scalatest"               %% "scalatest"               % "3.2.14",
     "org.scalatestplus.play"      %% "scalatestplus-play"      % "5.1.0",
     "org.pegdown"                 %  "pegdown"                 % "1.6.0",
-    "org.jsoup"                   %  "jsoup"                   % "1.10.3",
+    "org.jsoup"                   %  "jsoup"                   % "1.15.3",
     "com.typesafe.play"           %% "play-test"               % PlayVersion.current,
-    "org.mockito"                 %% "mockito-scala"           % "1.16.34" ,
-    "org.scalatestplus"           %% "scalatestplus-scalacheck"  % "3.1.0.0-RC2",
-    "com.github.tomakehurst"      %  "wiremock-standalone"     % "2.27.0",
+    "org.mockito"                 %% "mockito-scala"           % "1.17.12" ,
+    "org.scalatestplus"           %% "scalacheck-1-17"           % "3.2.14.0",
+    "com.github.tomakehurst"      %  "wiremock-standalone"     % "2.27.2",
     "wolfendale"                  %% "scalacheck-gen-regexp"   % "0.1.2",
-    "uk.gov.hmrc.mongo"           %% "hmrc-mongo-test-play-28" % "0.68.0"
+    "uk.gov.hmrc.mongo"           %% "hmrc-mongo-test-play-28" % "0.74.0",
+    "com.vladsch.flexmark"        %  "flexmark-all"              % "0.64.0"
   ).map(_ % Test)
 
   def apply(): Seq[ModuleID] = compile ++ test
